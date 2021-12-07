9171e3244c9e812fc9f9fbb2929e164d255bbf4c
zittoooo <stellar7484@naver.com>
2021-12-07 18:37:00 +0900

    검색결과 1

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,13 +1,13 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, { searchFormView }) {
+  constructor(store, { searchFormView, searchResultView }) {
     console.log(tag, "constructor");
 
     this.store = store;
 
     this.searchFormView = searchFormView;
-    // TODO
+    this.searchResultView = searchResultView;
 
     this.subscribeViewEvents();
   }
@@ -20,10 +20,20 @@ export default class Controller {
 
   search(searchKeyword) {
     console.log(tag, "search", searchKeyword);
-    // TODO
+    this.store.search(searchKeyword);
+    this.render();
   }
 
   reset() {
     console.log(tag, "reset");
+  }
+
+  render() {
+    if (this.store.searchKeyword.length > 0) {
+      this.searchResultView.show(this.store.searchResult);
+      return ;
+    }
+
+    this.searchResultView.hide();
   }
 }
