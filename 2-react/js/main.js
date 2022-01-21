c97c4044ed6947836d0aac81a78d8229a7a9f1e6
zittoooo <stellar7484@naver.com>
2022-01-21 20:15:48 +0900

    검색 결과 초기화

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -29,7 +29,7 @@ class App extends React.Component {
   handleReset() {
     // this.setState({searchKeyword: ""});
     this.setState(()=> {
-      return {searchKeyword: ""};
+      return {searchKeyword: "", submitted: false};
     }, ()=> {
       console.log('ToDo: handleReset', this.state.searchKeyword);
     });
@@ -38,7 +38,7 @@ class App extends React.Component {
   handleChangeInput(event) {
     const searchKeyword = event.target.value;
 
-    if (searchKeyword.length <= 0) {
+    if (searchKeyword.length <= 0 && this.state.submitted) {
       return this.handleReset();
     }
 
